e9188c8faedf38edff1623af3b53a670154d802a
chamikasrimantha <chamikasrimantha2002@gmail.com>
2024-12-31 19:05:21 +0530

    changes

--- lib/controllers/AchievementsController.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AchievementsController extends ChangeNotifier {
-
-}
--- lib/controllers/ActivityController.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ActivityController extends ChangeNotifier {
-  
-}
