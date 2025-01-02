6a82c036185439f782991af541beaff778d0b769
chamikasrimantha <chamikasrimantha2002@gmail.com>
2025-01-02 19:54:24 +0530

    changes

--- lib/screens/user/article/articles_screen.dart
+++ lib/screens/user/article/articles_screen.dart
@@ -1,4 +1,4 @@
-import 'package:application/screens/user/activity/view_article_screen.dart';
+import 'package:application/screens/user/article/view_article_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
